2c1b20a63589dac944ea4d59ef9698bec3654e2e
Felipe Nobre <nobrefelipe1@gmail.com>
2021-10-25 18:50:12 +0100

    first commit

---
+++ app/android/app/src/main/kotlin/app/vercel/felipenobre/clubbersapp/clubbers_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.vercel.felipenobre.clubbersapp.clubbers_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ clubber/README.md
@@ -0,0 +1,5 @@
+## Clubber
+
+Clubber is the UI kit for the Clubbers App.
+Here will be all the widgets and branding guidelines used accross the main app.
+
