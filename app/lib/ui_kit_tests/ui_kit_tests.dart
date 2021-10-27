d0919a3d0b7efb80128c481c2597c405e186b142
Felipe Nobre <nobrefelipe1@gmail.com>
2021-10-27 18:44:50 +0100

    create ui kit preview screen

---
+++ app/lib/ui_kit_tests/ui_kit_tests.dart
@@ -0,0 +1,52 @@
+import 'package:clubber/clubber.dart';
+import 'package:flutter/material.dart';
+
+class UiKitTests extends StatelessWidget {
+  const UiKitTests({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Clubber UI Kit'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ///
+            /// BUTTONS
+            ///
+            CLButton.withShadow(
+              label: 'Solid shadow button',
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20),
+            CLButton.solid(
+              label: 'Solid button',
+              color: CLPalette.darkGrey,
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20),
+            CLButton.outlined(
+              label: 'Outlined button',
+              onPressed: () {},
+            ),
+            const SizedBox(height: 20),
+
+            ///
+            /// EVENT HERO ITEM
+            ///
+            CLEventHeroItem(
+              title: 'Vintage Culture',
+              subtitle: 'Fabric - London',
+              image: 'https://edmhousenetwork.com/wp-content/uploads/2020/03/zvintage.png',
+              onTap: () {
+                print('lksandnjdnsa');
+              },
+            ),
+          ],
+        ).marginAll(30),
+      ),
+    );
+  }
+}
